8624febe7921e586839ed27cea9b6a62c7f9faef
JoePro <git@joeproit.com>
2023-06-27 03:27:51 -0500

    frame

---
+++ src/app/shared/SADCOPS.js
@@ -0,0 +1,133 @@
+import sadcopalphaImg from '../assets/img/react-lake.jpg';
+import sadcop1Img from '../assets/img/react-lake.jpg';
+import sadcop2Img from '../assets/img/react-lake.jpg';
+import sadcop3Img from '../assets/img/react-lake.jpg';
+import sadcop4Img from '../assets/img/react-lake.jpg';
+import sadcop5Img from '../assets/img/react-lake.jpg';
+import sadcop6Img from '../assets/img/react-lake.jpg';
+import sadcop7Img from '../assets/img/react-lake.jpg';
+import sadcop8Img from '../assets/img/react-lake.jpg';
+import sadcop9Img from '../assets/img/react-lake.jpg';
+import sadcop10Img from '../assets/img/react-lake.jpg';
+import sadcop11Img from '../assets/img/react-lake.jpg';
+import sadcop12Img from '../assets/img/react-lake.jpg';
+
+export const SADCOPS = [
+    {
+        id: 0,
+        name: 'Sad Cop Alpha',
+        image: sadcopalphaImg,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The original Sad Cop.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 1,
+        name: 'Sad Cop 1',
+        image: sadcop1Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The first of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 2,
+        name: 'Sad Cop 2',
+        image: sadcop2Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The second of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 3,
+        name: 'Sad Cop 3',
+        image: sadcop3Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The third of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 4,
+        name: 'Sad Cop 4',
+        image: sadcop4Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The fourth of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 5,
+        name: 'Sad Cop 5',
+        image: sadcop5Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The fifth of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 6,
+        name: 'Sad Cop 6',
+        image: sadcop6Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The sixth of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 7,
+        name: 'Sad Cop 7',
+        image: sadcop7Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The seventh of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 8,
+        name: 'Sad Cop 8',
+        image: sadcop8Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The eighth of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 9,
+        name: 'Sad Cop 9',
+        image: sadcop9Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The ninth of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 10,
+        name: 'Sad Cop 10',
+        image: sadcop10Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The tenth of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 11,
+        name: 'Sad Cop 11',
+        image: sadcop11Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The eleventh of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    },
+    {
+        id: 12,
+        name: 'Sad Cop 12',
+        image: sadcop12Img,
+        release: '2020-01-01',
+        featured: false,
+        description:
+            'The twelfth of the Sad Cops.  He is the first of his kind.  He is the Alpha.'
+    }
+];
